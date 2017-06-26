547b0b55dd9efe2244ff704029e9607b9764c054
mitsuo <mitsuo_h@outlook.com>
2017-06-26 18:38:44 +0900

    Initial commit

---
+++ client.py
@@ -0,0 +1,75 @@
+import socket
+import sys
+import requests
+from urllib.parse import urlparse
+
+
+def main():
+    argv = sys.argv
+    argc = len(argv)
+    buf_size = 4096
+
+    url = argv[1]
+    hr = requests.head(url)
+    length = int(hr.headers['Content-Length'])
+
+    # print(hr.headers)
+
+    url = urlparse(argv[1])
+    port = int(argv[2])
+    num = int(argv[3])
+
+    chunk_size = int(length / num)
+    reminder = int(length % num)
+
+    sock = []
+
+    address = (socket.gethostbyname(url.netloc), port)
+
+    s = socket.create_connection(address)
+    msg = set_message(url, 0, 100)
+    s.sendall(msg.encode())
+    r = s.recv(buf_size).decode()
+    print(r)
+    n = get_order(r, num, length, chunk_size)
+    print(n)
+    s.close()
+
+    # for s in range(num):
+    #     s = socket.create_connection(address)
+    #     sock.append(s)
+    #
+    # begin = 0
+    # data = []
+    #
+    # for s in sock:
+    #     msg = set_message(url, begin, begin + chunk_size - 1)
+    #     begin += chunk_size
+    #     s.sendall(msg.encode())
+    #     r = s.recv(buf_size).decode()
+    #     index = get_order(r, num, length)
+    #     c = r[r.find('\r\n\r\n') + len('\r\n\r\n'):]
+    #     data.insert(index, c)
+    #     print(c)
+
+
+def set_message(url, n, m):
+    return 'GET {0} HTTP/1.1\r\nHost: {1}\r\nRange: bytes={2}-{3}\r\n\r\n'.format(url.path, url.netloc, n, m)
+
+
+def get_order(r, num, length, chunk_size):
+    r = r[r.find('bytes ') + len('bytes '):]
+    r = r[:r.find('\r\n\r\n')]
+
+    x = ''
+    for i in r:
+        if i == '-':
+            break
+
+        x += i
+
+    return x
+
+
+if __name__ == '__main__':
+    main()
