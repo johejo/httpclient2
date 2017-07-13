16152958e9ea6138090cc5fe4665880e15bcc6ea
mitsuo <mitsuo_h@outlook.com>
2017-07-13 17:03:15 +0900

    test

--- movie_play.py
+++ movie_play.py
@@ -3,6 +3,6 @@ import vlc
 p = vlc.MediaPlayer('sample.mp4')
 p.play()
 while True:
-    q = input('Input \'q\' to quit.')
+    q = input('Input \'q\' to quit.\n>>')
     if q == 'q':
         break
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 python client.py http://165.242.111.73/sample.mp4 5 &
+sleep 3m
 python movie_play.py
